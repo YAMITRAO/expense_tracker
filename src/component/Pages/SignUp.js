a14dcd8ee185e6ac035fd30787a66a24a1d7632e
YAMITRAO <work.aky@gmail.com>
2024-03-30 14:20:30 +0530

    login page added

--- src/component/Pages/SignUp.js
+++ src/component/Pages/SignUp.js
@@ -1,14 +1,10 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import style from "./SignUp.module.css"
 
-const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDwcYCFrLAPoOvfWZN6fmD6d8Luyojx3Fw'
-
-
+const signUpurl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDwcYCFrLAPoOvfWZN6fmD6d8Luyojx3Fw';
+const loginurl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDwcYCFrLAPoOvfWZN6fmD6d8Luyojx3Fw';
 const SignUp = () => {
 
-  const emailInputRef = useRef();
-  const passwordInputRef = useRef();
-  const confirmPasswordInputRef = useRef();
 
   const [errorMsg, setErrorMsg] = useState();
   const [successMsg, setSuccessMsg] = useState();
@@ -16,11 +12,13 @@ const SignUp = () => {
   const [emailValue, setEmailValue] = useState();
   const [passwordValue, setPaswordValue] = useState();
   const [confirmPasswordValue, setConfirmPasswordValue] = useState();
+
+  const [isSignUp, setIsSignUp] = useState(true);
   
 
   const signUpApi = async(email, pass) =>{
     try{
-      const response = await fetch(url, {
+      const response = await fetch(signUpurl, {
         method:"POST",
         body:JSON.stringify({
           email:email,
@@ -40,7 +38,10 @@ const SignUp = () => {
       }
       const data = await response.json();
       data && setSuccessMsg("SuccessFully SignUp")
-      data &&  setTimeout( () => {setSuccessMsg()}, 1500)
+      data &&  setTimeout( () => {
+        setSuccessMsg();
+        setIsSignUp(false);
+      }, 1500)
       setEmailValue( "");
       setPaswordValue("");
       setConfirmPasswordValue("");
@@ -55,12 +56,44 @@ const SignUp = () => {
     }
   }
 
-  const formSubmitHandler = (e ) => {
-    e.preventDefault();
-    // const enteredEmail = emailInputRef.current.value;
-    // const enteredPassword = passwordInputRef.current.value;
-    // const enteredConfirmPassword = confirmPasswordInputRef.current.value;
+  const loginApi = async(email,pass) => {
+    try{
+    const response = await fetch(loginurl,{
+      method:"POST",
+      body:JSON.stringify({
+        email:email,
+        password:pass,
+        returnSecureToken:true,
+      })
+    })
 
+    if(!response.ok){
+      const data = await response.json();
+        console.log(data);
+        throw new Error(data.error.message);
+    }
+
+    const data = await response.json();
+      data && setSuccessMsg("Login Successfully")
+      data &&  setTimeout( () => {
+        setSuccessMsg();
+        setIsSignUp(false);
+      }, 1500)
+    setEmailValue("");
+    setPaswordValue("");
+    setTimeout( () => {  setSuccessMsg()},1500)
+  }
+  catch(error){
+    console.log("AUTH_ERROR", error);
+    setErrorMsg(`AUTH_ERROR ${error}`);
+    setTimeout(()=> {setErrorMsg()}, 1500)
+  }
+   
+  }
+
+
+  const formSignUpSubmitHandler = (e) => {
+    e.preventDefault();
     const enteredEmail = emailValue
     const enteredPassword = passwordValue
     const enteredConfirmPassword = confirmPasswordValue
@@ -82,30 +115,47 @@ const SignUp = () => {
     
   }
 
+  const formLoginSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailValue
+    const enteredPassword = passwordValue
+    if(enteredPassword.lenght < 6){
+      setErrorMsg("Password must be 6 characters long");
+      setTimeout(() => {
+        setErrorMsg();
+      }, 1500);
+      return
+    }
+    loginApi(enteredEmail, enteredPassword);
+  }
+
   return (
    <div className={style.container}>
          <div className={style.formContainer}>
-          <h1>SignUp</h1>
-          <form onSubmit={ formSubmitHandler}> 
+          {isSignUp && <h1>SignUp</h1>}
+          {!isSignUp && <h1>Login</h1>}
+          isSignUp && <form onSubmit={isSignUp ?formSignUpSubmitHandler : formLoginSubmitHandler}> 
             <div className={style.inputContainer}>
 
               <div className={style.emailContainer}>
                 <label >Email<sup>*</sup></label>
-                <input type="email"  placeholder='Enter email' ref={emailInputRef} onChange={(e) => setEmailValue(e.target.value)} value={emailValue} required/>
+                <input type="email"  placeholder='Enter email'  onChange={(e) => setEmailValue(e.target.value)} value={emailValue} required/>
               </div>
 
               <div className={style.passwordContainer}>
                 <label >Password<sup>*</sup></label>
-                <input type="password"  placeholder='Enter password ' ref={passwordInputRef} onChange={(e) => setPaswordValue(e.target.value)} value={passwordValue} required/>
+                <input type="password"  placeholder='Enter password '  onChange={(e) => setPaswordValue(e.target.value)} value={passwordValue} required/>
               </div>
 
-              <div className={style.confirmPasswordContainer}>
+              {isSignUp && <div className={style.confirmPasswordContainer}>
                 <label htmlFor='password'>Confirm Password<sup>*</sup></label>
-                <input type="password"  placeholder='Re-enter password 'ref={confirmPasswordInputRef} required onChange={ (e)=> setConfirmPasswordValue(e.target.value)} value={confirmPasswordValue}/>
-              </div>
+                <input type="password"  placeholder='Re-enter password ' required onChange={ (e)=> setConfirmPasswordValue(e.target.value)} value={confirmPasswordValue}/>
+              </div>}
 
               <div className={style.submitButton}>
-                <button type="submit">SignUp</button>
+                {isSignUp && <button>SignUp</button>}
+                {!isSignUp && <button >LogIn</button>}
               </div>
             </div>
           </form>
@@ -113,6 +163,9 @@ const SignUp = () => {
           {errorMsg} {successMsg}
           </div>
          </div>
+
+         {isSignUp && <div className={style.haveLogin}>Have an Account? <button onClick={()=> setIsSignUp(false)}>Login</button></div>}
+         {!isSignUp && <div className={style.haveSignUp}>Do't Have Account? <button onClick={()=> setIsSignUp(true)}>SignUp</button></div>}
    </div>
   )
 }
